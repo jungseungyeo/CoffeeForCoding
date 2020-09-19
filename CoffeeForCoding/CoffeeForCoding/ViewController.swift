edf61130ac1ed1d734d153757aff78835f66abca
jungseungyeo <duwjdtmd91@gmail.com>
2020-09-19 23:46:20 +0900

    Feat: Project init
    프로젝트 생성

---
+++ CoffeeForCoding/CoffeeForCoding/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CoffeeForCoding
+//
+//  Created by linsaeng on 2020/09/19.
+//  Copyright © 2020 linsaeng. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
